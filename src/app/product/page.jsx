831a453f7454b02b6d0f2cda6a8a038013e2e732
Arvindkumar7742 <arvindkumar14032004@gmail.com>
2024-09-28 22:59:21 +0530

    Library for react hot toast and react icons added

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -3,12 +3,14 @@
 import { Provider } from 'react-redux';
 import store from '@/redux/store';
 import { MainComponent } from './components/MainComponent';
+import { Toaster } from 'react-hot-toast';
 
 
 export default function Planet() {
   return (
     <Provider store={store}>
         <MainComponent/>
+        <Toaster />
     </Provider>
   );
 }
